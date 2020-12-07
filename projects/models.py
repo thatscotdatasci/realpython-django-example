5cfcd2c9544055340be28e453d113eb1d38168f4
Alan Clark <alan@thatscotdatasci.com>
2020-12-07 15:41:24 +0000

    Tutorial correction - FilePathField needs abs path

--- projects/models.py
+++ projects/models.py
@@ -1,13 +1,20 @@
+import os
+
 from django.db import models
+from django.conf import settings
 
 # Create your models here.
+
+
+def get_img_abs_path():
+    return os.path.join(settings.BASE_DIR, "projects/static/projects/img")
 
 
 class Project(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField()
     technology = models.CharField(max_length=20)
-    image = models.FilePathField(path="/projects/img")
+    image = models.FilePathField(path=get_img_abs_path)
 
     def __repr__(self):
         return self.title
