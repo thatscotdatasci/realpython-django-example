e558baa8ebbf3ea461f888eb89c4a30f1db54371
Alan Clark <alan@thatscotdatasci.com>
2020-12-07 15:11:15 +0000

    Model and migration correction

--- projects/migrations/0002_img_path_refactor.py
+++ projects/migrations/0002_img_path_refactor.py
@@ -13,6 +13,6 @@ class Migration(migrations.Migration):
         migrations.AlterField(
             model_name='project',
             name='image',
-            field=models.FilePathField(path='projects/img'),
+            field=models.FilePathField(path='/projects/img'),
         ),
     ]
--- projects/models.py
+++ projects/models.py
@@ -7,7 +7,7 @@ class Project(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField()
     technology = models.CharField(max_length=20)
-    image = models.FilePathField(path="projects/img")
+    image = models.FilePathField(path="/projects/img")
 
     def __repr__(self):
         return self.title
