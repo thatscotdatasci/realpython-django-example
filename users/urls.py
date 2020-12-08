065e8b7f4fc9a4aae2471ea5799fc470d354e24e
Alan Clark <alan@thatscotdatasci.com>
2020-12-08 16:43:57 +0000

    Adding some context to the issue being resolved

--- users/urls.py
+++ users/urls.py
@@ -7,5 +7,6 @@ app_name = "users"
 urlpatterns = [
     path('dashboard/', views.dashboard, name="dashboard"),
     # NOTE: Ensure this is in the urls.py of the configuration app
+    # https://stackoverflow.com/questions/30821412/reverse-for-password-change-done-with-arguments-and-keyword-arguments
     # path('users/accounts/', include("django.contrib.auth.urls")),
 ]
