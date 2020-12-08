e1ec69e645a210a5171b57b56d7f228f1b18298d
Alan Clark <alan@thatscotdatasci.com>
2020-12-08 11:14:56 +0000

    Switching to using postrges database with persisted data

--- portfolio/settings_docker.py
+++ portfolio/settings_docker.py
@@ -1,7 +1,18 @@
 import os
 from .settings import *
 
-CACHES['default'].update({'LOCATION': os.environ.get("REDIS_URL")})
+CACHES['default'].update({'LOCATION': os.environ.get('REDIS_URL')})
 
 # https://stackoverflow.com/questions/26898597/django-debug-toolbar-and-docker
 DEBUG_TOOLBAR_CONFIG.update({'SHOW_TOOLBAR_CALLBACK': lambda x: True})
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get("POSTGRES_DB"),
+        'USER': os.environ.get("POSTGRES_USER"),
+        'PASSWORD': os.environ.get("POSTGRES_PASSWORD"),
+        'HOST': os.environ.get("POSTGRES_HOSTNAME"),
+        'PORT': '5432',
+    }
+}
